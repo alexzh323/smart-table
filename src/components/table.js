084fab35ee22a0242b859d1301ea286adbf01c9f
Ixidor <alexzh323@gmail.com>
2026-05-17 06:05:02 +0300

    1 step

--- src/components/table.js
+++ src/components/table.js
@@ -10,16 +10,42 @@ import {cloneTemplate} from "../lib/utils.js";
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
+  
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    after.forEach(subName => {                            // перебираем нужный массив идентификаторов
+      root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+      root.container.append(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+    });
+
+     before.reverse().forEach(subName => {                            // перебираем нужный массив идентификаторов
+      root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+      root.container.prepend(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener("change", onAction);
+    root.container.addEventListener("reset", () => {
+        setTimeout(onAction, 10)});
+    root.container.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+        const nextRows = data.map(item => { 
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => { 
+                if(row.elements[key]) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+            return row.container;
+        });
+        
+        
+    };
 
     return {...root, render};
 }
